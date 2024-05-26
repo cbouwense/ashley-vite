38f91ee56efd65def20503fa9d67429a452f43c1
Christian Bouwense <bouwensec@gmail.com>
2024-05-25 20:17:06 -0400

    got the nice div on mobile

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+        spacing: {
+            '10vw': '10vw',
+            '25vw': '25vw',
+            '33vw': '33vw',
+            '50vw': '50vw',
+            '67vw': '67vw',
+            '75vw': '75vw',
+            '100vw': '100vw',
+            '10vh': '10vh',
+            '25vh': '25vh',
+            '33vh': '33vh',
+            '50vh': '50vh',
+            '67vh': '67vh',
+            '75vh': '75vh',
+            '100vh': '100vh',
+          }
+    },
+  },
+  plugins: [],
+}
+
